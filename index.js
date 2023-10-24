e44f96ce4c7a38060d749e17ca16dd911e93a63f
k2 <remoxylin@gmail.com>
2023-10-24 01:31:19 -0700

    crete first route then controller

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const port = 1947;
 
-function requestHandler(req, res){
-    console.log(`req`);
-    res.end(`mera to ho gya`);
-}
+//USE EXPRESS ROUTER
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
